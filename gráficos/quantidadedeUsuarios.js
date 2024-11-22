261626270eda7834f68aa41d164df2e4df8b4aee
moeilijkluis <112392785+moeilijkluis@users.noreply.github.com>
2024-11-22 19:34:14 -0300

    22/11

--- gráficos/quantidadedeUsuarios.js
+++ gráficos/quantidadedeUsuarios.js
@@ -1,60 +1,3 @@
-const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
-}
-
-const tickConfig = {
-    family: getCSS('--font'),
-    size: 16,
-    color: getCSS('--primary-color')
-}
-
-export {getCSS, tickConfig}
-
-
-
-
-
-
-
-
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
-async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
-
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
-
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
-}
-
-vizualizarInformacoesGlobais()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
@@ -114,4 +57,3 @@ async function quantidadeUsuariosPorRede() {
 }
 
 quantidadeUsuariosPorRede()
-
